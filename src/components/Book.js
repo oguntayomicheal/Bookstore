74f180e5b363ae0cd86852c9fdf20d68c9be04c2
Micheal <oguntayomicheal@gmail.com>
2022-11-16 23:27:26 +0100

    Add functionality to remove  books

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,4 +1,8 @@
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
+
 function Book(props) {
+  const dispatch = useDispatch();
   const books = { props };
   const eachBook = books.props.book;
 
@@ -12,7 +16,14 @@ function Book(props) {
         <em>{eachBook.author}</em>
       </span>
       {' '}
-      <button type="button">Delete</button>
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(removeBook(eachBook.id));
+        }}
+      >
+        Delete
+      </button>
     </li>
   );
 }
